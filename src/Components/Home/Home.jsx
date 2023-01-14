4180e8b432d45329e2466db7a1781db4850e65e2
xmnnasrul <mygithub.nasrul@gmail.com>
2023-01-14 16:01:37 +0700

    add swiiperJs

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import Logo from './../Assets/logo.png'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
+
+import Logo from './../Assets/logo.jpeg'
 import './Home.css'
 function Home() {
+
     return (
         <>
-            <div className="Home-Content">
-                <img id='Logo' src={Logo} alt="#" />
-            </div>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                <SwiperSlide><img src={Logo} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={Logo} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={Logo} alt="" /></SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
         </>
     )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-// import './Navbar.css'
+import './Navbar.css'
 import { Link } from 'react-router-dom';
-import Logo from './../Assets/logo.png'
 import { Navbar, Nav, Container } from 'react-bootstrap';
 function Navbarr() {
     return (
