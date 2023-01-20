a2c537537bcde16bfb83721b440e388b08c17e38
xmnnasrul <mygithub.nasrul@gmail.com>
2023-01-21 01:19:29 +0700

    add butto

--- src/Components/Jasa/Jasa.jsx
+++ src/Components/Jasa/Jasa.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from './../Footer/Footer'
 import './Jasa.css'
 function Jasa() {
+    const [selectedOptionPaket, setSelectedOptionPaket] = useState('');
+
+    const handleChangePaket = (e) => {
+        setSelectedOptionPaket(e.target.value);
+    }
+    const [selectedOptionBintang, setSelectedOptionBintang] = useState('');
+
+    const handleChangeBintang = (e) => {
+        setSelectedOptionBintang(e.target.value);
+    }
     return (
-        <>
+        <div className='body'>
             <div className="Jasa">
 
                 <div className="Title">
@@ -19,14 +29,22 @@ function Jasa() {
                         </div>
                         <div className='Card-Center'>
                             <form action="#">
-                                <input type="text" placeholder='Username' />
+                                <input className='input' type="text" placeholder='Username' />
                                 <div className="Server">
-                                    <input type="number" placeholder='ID' />
-                                    <input type="number" placeholder='Server' />
+                                    <input className='input' type="tel" placeholder='ID' />
+                                    <input className='input' type="tel" placeholder='Server' />
                                 </div>
-                                <input type="text" placeholder='Pilih Rank' /><br></br>
-                                <div className="Submit">
-                                    <button >Submit</button >
+                                <div>
+                                    <select value={selectedOptionPaket} onChange={handleChangePaket} className="custom-select">
+                                        <option value="option1">Pilih Rank</option>
+                                        <option value="option2">Master-GM</option>
+                                        <option value="option3">GM-Epic</option>
+                                        <option value="option4">Epic-Legend</option>
+                                        <option value="option5">Legend-Mythic</option>
+                                    </select>
+                                </div>
+                                <div className="Submit ">
+                                    <button className='button-80' >Submit</button >
                                 </div>
                                 <button style={{ display: "none" }}>
                                     <a href="#" className='Pembayaran'>Wa</a>
@@ -41,16 +59,24 @@ function Jasa() {
                         </div>
                         <div className='Card-Center'>
                             <form action="#">
-                                <input type="text" placeholder='Username' />
+                                <input className='input' type="text" placeholder='Username' />
                                 <div className="Server">
-                                    <input type="number" placeholder='ID' />
-                                    <input type="number" placeholder='Server' />
+                                    <input className='input' type="tel" placeholder='ID' />
+                                    <input className='input' type="tel" placeholder='Server' />
                                 </div>
-                                <input type="text" placeholder='Rank' />
+                                <select value={selectedOptionBintang} style={{ marginBottom: "5px" }} onChange={handleChangeBintang} className="custom-select">
+                                    <option>Pilih Rank</option>
+                                    <option value="Elite">Elite</option>
+                                    <option value="option3">Master</option>
+                                    <option value="option4">Grandmaster</option>
+                                    <option value="option5">Epic</option>
+                                    <option value="option6">Legend</option>
+                                    <option value="option7">Mythic</option>
+                                </select>
                                 <br></br>
-                                <input type="text" placeholder='Jumlah Bintang' />
+                                <input className='input' type="tel" placeholder='Jumlah Bintang' />
                                 <div className="Submit">
-                                    <button >Submit</button >
+                                    <button className='button-80' >Submit</button >
                                 </div>
                                 <button style={{ display: "none" }}>
                                     <a href="#" className='Pembayaran'>Wa</a>
@@ -62,9 +88,9 @@ function Jasa() {
 
                 </div>
 
-            </div>
+            </div >
             <Footer />
-        </>
+        </div >
     )
 }
 
